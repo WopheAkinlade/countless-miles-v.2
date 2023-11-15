2213587a5e28b7b6d54baf5361b80d888b8c488b
Wophe <wophe.akinlade@gmail.com>
2023-11-15 19:45:18 +0100

    Created the text layout of the hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,35 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <nav>
         <img src="../assets/countlessmiles.svg" alt="Logo" className="logo" />
+        <ul>
+          <a href="#about">
+            <li className="nav-link">About Us</li>
+          </a>
+          <a href="#blog">
+            <li className="nav-link">Blog</li>
+          </a>
+          <a href="#contact">
+            <li className="nav-link">Contact Us</li>
+          </a>
+        </ul>
+        <button className="nav-button">GET EARLY ACCESS</button>
       </nav>
+
+      <div className="hero-textbox">
+        <h1 className="hero-title">
+          If You Love Travel Travel Freedom is Here
+        </h1>
+        <div className="barrier" />
+        <h3 className="hero-text">
+          Experience Travel As A Service With Our End-to-End Trip Planning
+          Platform.
+        </h3>
+        <button className="hero-btn">GET EARLY ACCESS</button>
+      </div>
     </>
   );
 };
