1d2f44d03dd5508a01759a0ecc76ea8192f0b53f
Wophe <wophe.akinlade@gmail.com>
2023-11-15 19:28:51 +0100

    Moved the assets into the src folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <div>This is the home component</div>
-      <Link to="about">Click to view our about page</Link> <br />
-      <Link to="contact">Click to view our contact page</Link>
+      <nav>
+        <img src="../assets/countlessmiles.svg" alt="Logo" className="logo" />
+      </nav>
     </>
   );
 };
