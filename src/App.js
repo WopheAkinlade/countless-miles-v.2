06470df11b564b6ed5a1cda5ea63236b6698e728
Wophe <wophe.akinlade@gmail.com>
2025-01-30 23:28:52 -0500

    Removed React router from the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Contacts from "./components/Contacts";
+
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contacts />} />
-      </Routes>
     </div>
   );
 }
