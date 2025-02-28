4e19ff24c47da58f26c57b8ff42d12ae998094d8
Wophe <wophe.akinlade@gmail.com>
2025-02-28 00:06:27 -0400

    Added the Navbar component to the home screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Home from "./sections/Home";
 
 
 function App() {
   return (
     <div className="App">
+      <Home />
     </div>
   );
 }
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,24 +1,10 @@
 import React from "react";
+import Navbar from "./Navbar/Navbar";
 
 const Home = () => {
   return (
     <>
-      <nav>
-        <img src="../assets/countlessmiles.svg" alt="Logo" className="logo" />
-        <ul>
-          <a href="#about">
-            <li className="nav-link">About Us</li>
-          </a>
-          <a href="#blog">
-            <li className="nav-link">Blog</li>
-          </a>
-          <a href="#contact">
-            <li className="nav-link">Contact Us</li>
-          </a>
-        </ul>
-        <button className="nav-button">GET EARLY ACCESS</button>
-      </nav>
-
+      <Navbar />
       <div className="hero-textbox">
         <h1 className="hero-title">
           If You Love Travel Travel Freedom is Here
